ae95693d960a5fe3a864b28f3d7e85ca7f9b0394
Mounish-T <tmounish02@gmail.com>
2025-08-14 21:40:05 +0530

    sample books added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function newUser(){
             <input placeholder="Enter your name" type="text" class="getName">
             <input placeholder="Enter password" type="password" class="getPwd">
             <div class="signin">
-                <a href="home/home.html">
+                <a href="admin/home/home.html">
                     <h4 style="background-color: blue;" onclick="signIn()">Create User</h4>
                 </a>
                 <h4 style="background-color: green;" onclick="oldUser()">Sign In</h4>
@@ -20,7 +20,7 @@ function oldUser(){
     input.innerHTML = `
         <input placeholder="Enter your mail" type="text" class="getMail">
         <div class="signin">
-            <a href="home/home.html">
+            <a href="admin/home/home.html">
                 <h4 style="background-color: blue;" onclick="signIn()">Sign In</h4>
             </a>
             <h4 style="background-color: green;" onclick="newUser()">New User</h4>
