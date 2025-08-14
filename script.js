81cd528a6fe9496553b21a2d5d2767c529d15abd
Mounish-T <tmounish02@gmail.com>
2025-08-14 11:40:57 +0530

    Home page navbar added

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ function newUser(){
             <input placeholder="Enter your name" type="text" class="getName">
             <input placeholder="Enter password" type="password" class="getPwd">
             <div class="signin">
-                <h4 style="background-color: blue;" onclick="">Create User</h4>
-                <h4 style="background-color: green;" onclick="signIn()">Sign In</h4>
+                <a href="home/home.html">
+                    <h4 style="background-color: blue;" onclick="signIn()">Create User</h4>
+                </a>
+                <h4 style="background-color: green;" onclick="oldUser()">Sign In</h4>
             </div>
         </form>    
     `;
 }
 
-function signIn(){
+function oldUser(){
     let input = document.getElementById("input-details");
     input.innerHTML = `
         <input placeholder="Enter your mail" type="text" class="getMail">
